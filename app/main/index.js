acf7accc17df7183ad30c46b8769a90b543e55b4
saad <saad.saeed@blueeast.com>
2017-06-14 14:02:28 +0500

    Menu configuration & Electron configuration

--- app/main/index.js
+++ app/main/index.js
@@ -1,48 +1,53 @@
 import { app, BrowserWindow } from "electron";
+import MenuBuilder from './menu';
 
 const PORT = 3000;
 
 let win;
 
-function createWindow() {
+const installExtensions = async () => {
+  const installer = require('electron-devtools-installer');
+  const forceDownload = !!process.env.UPGRADE_EXTENSIONS;
+  const extensions = [
+    'REACT_DEVELOPER_TOOLS',
+    'REDUX_DEVTOOLS'
+  ];
+
+  return Promise
+    .all(extensions.map(name => installer.default(installer[name], forceDownload)))
+    .catch(console.log);
+};
+
+app.on("ready", async () => {
   win = new BrowserWindow({
     show: false,
-    width: 800,
-    height: 600
+    width: 1024,
+    height: 728
   });
+  if (process.env.NODE_ENV === 'development' || process.env.DEBUG_PROD === 'true') {
+    await installExtensions();
+  }
 
   if (process.env.NODE_ENV === "development") {
-    // eslint-disable-next-line node/no-unpublished-require
     require("electron-debug")();
     win.loadURL(`http://localhost:${PORT}/build/index.html`);
   } else {
     win.loadURL(`file://${__dirname}/index.html`);
   }
 
-  win.webContents.on("did-finish-load", () => {
+  win.webContents.on('did-finish-load', () => {
+    if (!win) {
+      throw new Error('"win" is not defined');
+    }
     win.show();
     win.focus();
   });
 
-  win.on("closed", () => {
+  win.on('closed', () => {
     win = null;
   });
-}
-
-app.on("ready", async () => {
-  if (process.env.NODE_ENV === "development") {
-    // eslint-disable-next-line node/no-unpublished-require
-    const installer = require("electron-devtools-installer");
-    const forceDownload = !!process.env.UPGRADE_EXTENSIONS;
-    const extensions = [
-      "REACT_DEVELOPER_TOOLS",
-      "REDUX_DEVTOOLS"
-    ];
-    await Promise.all(
-      extensions.map(e => installer.default(installer[e], forceDownload))
-    );
-  }
-  createWindow();
+  const menuBuilder = new MenuBuilder(win);
+  menuBuilder.buildMenu();
 });
 
 app.on("window-all-closed", () => {
@@ -51,8 +56,8 @@ app.on("window-all-closed", () => {
   }
 });
 
-app.on("activate", () => {
-  if (win === null) {
-    createWindow();
-  }
-});
+// app.on("activate", () => {
+//   if (win === null) {
+//     createWindow();
+//   }
+// });
